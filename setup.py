6cfb2f6c8af363d6033edc5a925299cde947bc36
Marian Domanski <mdomanski@usgs.gov>
2019-05-17 15:06:21 -0500

    update development environment

--- setup.py
+++ setup.py
@@ -1,8 +1,18 @@
-from distutils.core import setup
+from setuptools import setup
+
+from sphinx.setup_command import BuildDoc
+
+name = 'DTSGUI'
+version = '1.0'
+release = '1.0.0'
+cmdclass = {'build_sphinx': BuildDoc}
+docs_source = 'docs/'
+docs_build_dir = 'docs/_build'
+docs_builder = 'html'
 
 setup(
-    name='dtsgui',
-    version='0.0.0dev1',
+    name=name,
+    version=release,
     packages=['dts'],
     url='https://github.com/mdomanski-usgs/DTSGUI',
     license='CC0 1.0',
@@ -10,9 +20,16 @@ setup(
     author_email='mdomanski@usgs.gov',
     description='Distributed Temperature Sensor GUI',
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
         'Programming Language :: Python 2.7'
     ],
     python_requires='==2.7.*',
+    command_options={
+        'build_sphinx': {
+            'project': ('setup.py', name),
+            'version': ('setup.py', version),
+            'release': ('setup.py', release),
+            'source_dir': ('setup.py', docs_source),
+            'build_dir': ('setup.py', docs_build_dir),
+            'builder': ('setup.py', docs_builder)}
+    }
 )
